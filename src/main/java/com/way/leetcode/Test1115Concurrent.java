5c4e4b0c721da160e3cbe546ef2440cfada4a47c
wayleung <610686949@qq.com>
2020-11-28 16:32:51 +0800

    多线程1115

---
+++ src/main/java/com/way/leetcode/Test1115Concurrent.java
@@ -0,0 +1,86 @@
+package com.way.leetcode;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author wayleung
+ * @description 我们提供一个类：
+ * <p>
+ * class FooBar {
+ * public void foo() {
+ * for (int i = 0; i < n; i++) {
+ * print("foo");
+ * }
+ * }
+ * <p>
+ * public void bar() {
+ * for (int i = 0; i < n; i++) {
+ * print("bar");
+ * }
+ * }
+ * }
+ * 两个不同的线程将会共用一个 FooBar实例。其中一个线程将会调用foo()方法，另一个线程将会调用bar()方法。
+ * <p>
+ * 请设计修改程序，以确保 "foobar" 被输出 n 次。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: n = 1
+ * 输出: "foobar"
+ * 解释: 这里有两个线程被异步启动。其中一个调用 foo() 方法, 另一个调用 bar() 方法，"foobar" 将被输出一次。
+ * 示例 2:
+ * <p>
+ * 输入: n = 2
+ * 输出: "foobarfoobar"
+ * 解释: "foobar" 将被输出两次。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/print-foobar-alternately
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2020-11-28 15:21:00
+ */
+public class Test1115Concurrent {
+
+}
+
+class FooBar {
+    private int n;
+    ReentrantLock lock = new ReentrantLock();
+    private static int flag = 1;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                if (flag == 2) {
+                    this.wait();
+                }
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                flag = 2;
+                this.notify();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                if (flag == 1) {
+                    this.wait();
+                }
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                flag = 1;
+                this.notify();
+            }
+        }
+    }
+}
