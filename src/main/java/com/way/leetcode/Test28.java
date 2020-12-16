ea903894f4de878aeb3b14951195e577650c035e
wayleung <610686949@qq.com>
2020-12-16 17:51:37 +0800

    28 kmp?

---
+++ src/main/java/com/way/leetcode/Test28.java
@@ -0,0 +1,15 @@
+package com.way.leetcode;
+
+/**
+ * @author wayleung
+ * @description
+ * @date 2020-12-16 16:34:05
+ */
+public class Test28 {
+
+
+    //KMP?
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
