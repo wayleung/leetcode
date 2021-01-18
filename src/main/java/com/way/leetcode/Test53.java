71017f6f13271b918c30c2f465cc63d9bfae1f84
wayleung <610686949@qq.com>
2021-01-18 11:09:15 +0800

    动态规划 遍历看是否有增益

---
+++ src/main/java/com/way/leetcode/Test53.java
@@ -0,0 +1,40 @@
+package com.way.leetcode;
+
+/**
+ * @author wayleung
+ * @description 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 进阶:
+ * <p>
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021-01-15 16:21:32
+ */
+public class Test53 {
+    public static void main(String[] args) {
+//        System.out.println(new Test53().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(new Test53().maxSubArray(new int[]{-1}));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(sum>0){
+                sum = sum+nums[i];
+            }else{
+                sum = nums[i];
+            }
+            max = Math.max(max,sum);
+        }
+        return max;
+    }
+}
